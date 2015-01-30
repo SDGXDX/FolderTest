3113611637f3a7c0c44f95d39a5961b4d3e31cf8
Pat O'Neill <Pat.Ryan.Oneill@gmail.com>
2015-01-30 11:32:24 -0500

    Recursive working

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
-// Normally I would split these up into controller & service directories, but this is a simple example.
-// Brevity and all that good stuff.
+// TODO: This is messy. Move to browserify/grunt workflow.
+
 (function iife() {
 
 
-    /// jQuery event handling
-/*
-    var $folders = $('.folder');
-    $folders.on('click' ,function() {
-        var $sub = $(this).first();
-        console.log($sub);
-        if ($sub.hasClass('ng-hide')) { $sub.removeClass('ng-hide'); $sub.addClass('ng-show'); }
-        else { $sub.removeClass('ng-show'); $sub.addClass('ng-show'); }
-
-    });
-*/
-
-
-
-
 /// ANGULAR
-
-var folderApp = angular.module('folderApp',[]);
+angular.module('Folder.Directives',[]);
+var folderApp = angular.module('folderApp',['Folder.Directives']);
 
 // CONTROLLERS
 folderApp.controller('folderController', ['$scope', 'folderService' , function folderController($scope, folderService) {
@@ -29,9 +14,11 @@ folderApp.controller('folderController', ['$scope', 'folderService' , function f
     folderService.getJson().then(
         function(data) {
             $scope.folders = data.data;
+            console.log($scope.folders);
         }
     );
 
+/* TODO: Refactor this to work with the directive
     for(var i=0; i<$scope.folders.length; i++ ) {
         $scope.folders[i].showSubFolders = false;
     }
@@ -43,6 +30,7 @@ folderApp.controller('folderController', ['$scope', 'folderService' , function f
     };
 
     console.log($scope.folders);
+    */
 
 }]);
 
@@ -51,13 +39,55 @@ folderApp.factory('folderService', ['$http',  function folderFactory($http) {
 
     return  {
         getJson: function() {
+         
             return $http.get('http://agileproapi.azurewebsites.net/api/MyFolders')
                 .success(function(json) { return json; })
                 .error(function(err)  { return err; })
+                 
         }
     }
 
 
 }]);
+
+// folder-list is a top level piece that contains lists of folder-items
+angular.module('Folder.Directives').directive('folderList', function () {
+    return {
+        restrict: "E",
+        replace: true,
+        // isolate scope for our current list of folders
+        // changed to list because <folder-list folder-list=> seems a bit awkward.
+        scope: {
+            list: '=list'
+        },
+        // setup the actual repeating for all folders
+        template: "<ul><folder ng-repeat='folder in list' folder='folder'></folder></ul>"
+    }
+});
+
+angular.module('Folder.Directives').directive('folder', function ($compile) {
+    return {
+        restrict: "E",
+        replace: true,
+        scope: {
+            folder: '='
+        },
+        template: "<li>{{folder.Name}}</li>",
+        link: function (scope, element, attrs) {
+            if (angular.isArray(scope.folder.Folders)) {
+                // Magic! if the Folders array exists tack a new element onto it
+                element.append("<folder-list list='folder.Folders'></folder-list>"); 
+                // Compile the element against the scope and calls/renders the folderList directive, force refresh
+                // TODO: I have concerns about performance here - perhaps transclusion would be better,
+                // or maybe transclusion just calls $compile. Look into that. 
+                // It is DOM manipulation, so link seems like the right place for now. 
+                $compile(element.contents())(scope)
+
+            }
+        }
+    }
+});
+
+
 
 })();
